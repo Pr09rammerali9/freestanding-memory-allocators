d8bbdfaccfdb53ff6435ce74022634ad2d5849b7
Pr09rammerali9 <hakamiali197@gmail.com>
2025-08-02 05:52:20 +0300

    Create balloc.c

---
+++ balloc.c
@@ -0,0 +1,53 @@
+#include <lib/alloc.h>
+
+static unsigned char pool[POOL_SIZE] = { 0 };
+
+static size_t off = 0;
+
+void *balloc(size_t size, size_t alignment) {
+
+    if (!size) //size is zero, this is similar to if (size == 0)
+        return NULL;
+
+    uintptr_t cur_addr = (uintptr_t)pool + off;
+
+    size_t padding = 0;
+
+    if (alignment > 0) {
+
+        size_t rem = cur_addr % alignment;
+
+        if (rem != 0)
+            padding = alignment - rem;
+    }
+
+    if (off + padding + size > POOL_SIZE)
+        return NULL;
+
+    unsigned char *alloced_ptr = pool + off + padding;
+
+    off += (padding + size);
+
+    return (void *)alloced_ptr;
+
+}
+
+void breset(void) {
+
+    off = 0;
+
+    memset(pool, 0, POOL_SIZE);
+
+}
+
+size_t bget_used(void) {
+
+    return off;
+
+}
+
+size_t bget_remaining(void) {
+
+    return POOL_SIZE - off;
+
+} 
