8642e473b0bdd332cb2bda2b8f3f158e4cdd75c1
Pr09rammerali9 <hakamiali197@gmail.com>
2025-08-07 05:22:05 +0300

    Update alloc.h

--- alloc.h
+++ alloc.h
@@ -23,11 +23,18 @@ void *palloc(size_t size);
 void *palloc_ali(size_t size, size_t alignment);
 void pfree(void *p);
 void pfree_ali(void *p);
+void pinit_lk(void (*lk)(void), void (*unlk)(void));
+void *palloc_lk(size_t size);
+void *palloc_ali_lk(size_t size, size_t alignment);
+void pfree_ali_lk(void *p);
 
 /*pb prefix for pool allocator with best fit algorthim*/
 void pbinit(void);
 void *pballoc(size_t size);
 void pbfree(void *p);
+void pbinit_lk(void (*lk)(void), void (*unlk)(void));
+void *pballoc_lk(size_t size);
+void pbfree_lk(void *p);
 
 void tlsf_init(void);
 void tlsf_init_lk(void (*lk)(void), void (*unlk)(void));
@@ -38,4 +45,5 @@ void *tlsf_alloc_ali_lk(size_t size, size_t alignment);
 void tlsf_free(void *p);
 void tlsf_free_lk(void *p);
 void tlsf_add_pool(void *mem_start, size_t mem_size);
+
 #endif
