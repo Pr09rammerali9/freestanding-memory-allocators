e026926a54da9dc5c0b46001c67dbdf42bf0d7ba
Pr09rammerali9 <hakamiali197@gmail.com>
2025-08-20 11:30:25 +0300

    Update tlsf_alloc.c

--- tlsf_alloc.c
+++ tlsf_alloc.c
@@ -4,28 +4,27 @@
 #define TLSF_SL_INDEX_COUNT 32
 
 #define POOL_SIZE_BYTES 8192
-static char memory_pool[POOL_SIZE_BYTES];
+static uint8_t memory_pool[POOL_SIZE_BYTES];
 
-#define MIN_BLOCK_SIZE (sizeof(blk_hdr_t) + sizeof(size_t))
 #define TLSF_MAGIC 0xFEEDC0DE
+#define MIN_BLOCK_SIZE (sizeof(blk_hdr_t) + sizeof(size_t))
 
 typedef struct block_header {
     size_t size;
+    size_t prev_size;
     struct block_header* next_free;
     struct block_header* prev_free;
-    struct block_header* prev_block;
 } blk_hdr_t;
 
 typedef struct tlsf_t {
     blk_hdr_t* sl_list[TLSF_FL_INDEX_COUNT][TLSF_SL_INDEX_COUNT];
     uint32_t fl_bitmap;
     uint32_t sl_bitmap[TLSF_FL_INDEX_COUNT];
+    void (*fl_locks[TLSF_FL_INDEX_COUNT])(void);
+    void (*fl_unlocks[TLSF_FL_INDEX_COUNT])(void);
 } tlsf_t;
 
 static tlsf_t tlsf_allocator;
-
-static void (*_tlsf_lock)(void) = NULL;
-static void (*_tlsf_unlock)(void) = NULL;
 
 static inline int find_free_ls(size_t size, int* fl, int* sl) {
     if (size == 0) return -1;
@@ -38,6 +37,8 @@ static void add_blk_to_free_ls(blk_hdr_t* blk) {
     int fl, sl;
     find_free_ls(blk->size, &fl, &sl);
 
+    if (tlsf_allocator.fl_locks[fl]) tlsf_allocator.fl_locks[fl]();
+
     blk->next_free = tlsf_allocator.sl_list[fl][sl];
     if (tlsf_allocator.sl_list[fl][sl] != NULL)
         tlsf_allocator.sl_list[fl][sl]->prev_free = blk;
@@ -46,11 +47,15 @@ static void add_blk_to_free_ls(blk_hdr_t* blk) {
 
     tlsf_allocator.fl_bitmap |= (1 << fl);
     tlsf_allocator.sl_bitmap[fl] |= (1 << sl);
+
+    if (tlsf_allocator.fl_unlocks[fl]) tlsf_allocator.fl_unlocks[fl]();
 }
 
 static void rm_blk_from_free_ls(blk_hdr_t* blk) {
     int fl, sl;
     find_free_ls(blk->size, &fl, &sl);
+
+    if (tlsf_allocator.fl_locks[fl]) tlsf_allocator.fl_locks[fl]();
 
     if (blk->prev_free)
         blk->prev_free->next_free = blk->next_free;
@@ -64,42 +69,40 @@ static void rm_blk_from_free_ls(blk_hdr_t* blk) {
         if (tlsf_allocator.sl_bitmap[fl] == 0)
             tlsf_allocator.fl_bitmap &= ~(1 << fl);
     }
+
+    if (tlsf_allocator.fl_unlocks[fl]) tlsf_allocator.fl_unlocks[fl]();
 }
 
 static inline blk_hdr_t* get_next_block(blk_hdr_t* blk) {
-    return (blk_hdr_t*)((char*)blk + blk->size);
+    return (blk_hdr_t*)((uintptr_t)blk + blk->size);
+}
+
+static inline blk_hdr_t* get_prev_block(blk_hdr_t* blk) {
+    return (blk_hdr_t*)((uintptr_t)blk - blk->prev_size);
 }
 
 static inline blk_hdr_t* split_blk(blk_hdr_t* blk, size_t split_size) {
-    size_t leftover_size = blk->size - split_size;
     blk->size = split_size;
-
     blk_hdr_t* leftover_blk = get_next_block(blk);
-    leftover_blk->size = leftover_size;
-
-    leftover_blk->prev_block = blk;
-    blk_hdr_t* next_block = get_next_block(leftover_blk);
-    next_block->prev_block = leftover_blk;
-
+    leftover_blk->prev_size = blk->size;
     return leftover_blk;
 }
 
 static inline blk_hdr_t* coalesce_blk(blk_hdr_t* blk) {
-    blk_hdr_t* prev_blk = blk->prev_block;
-    if (prev_blk != NULL && get_next_block(prev_blk) == blk) {
+    blk_hdr_t* prev_blk = get_prev_block(blk);
+    if (prev_blk != NULL && prev_blk->size == blk->prev_size) {
         rm_blk_from_free_ls(prev_blk);
         blk->size += prev_blk->size;
-        blk->prev_block = prev_blk->prev_block;
         blk = prev_blk;
     }
 
     blk_hdr_t* next_blk = get_next_block(blk);
-    if (next_blk != NULL && next_blk->prev_block == blk) {
+    if (next_blk != NULL && next_blk->size > 0 && next_blk->prev_size == blk->size) {
         rm_blk_from_free_ls(next_blk);
         blk->size += next_blk->size;
-        get_next_block(blk)->prev_block = blk;
     }
-
+    get_next_block(blk)->prev_size = blk->size;
+    
     return blk;
 }
 
@@ -108,12 +111,9 @@ static void tlsf_add_pool_internal(void* mem_start, size_t mem_size) {
 
     blk_hdr_t* initial_blk = (blk_hdr_t*)mem_start;
     initial_blk->size = mem_size;
-    initial_blk->prev_block = NULL;
-
-    blk_hdr_t* next_block = get_next_block(initial_blk);
-    next_block->prev_block = initial_blk;
-
+    initial_blk->prev_size = 0;
     add_blk_to_free_ls(initial_blk);
+    get_next_block(initial_blk)->prev_size = initial_blk->size;
 }
 
 void tlsf_add_pool(void* mem_start, size_t mem_size) {
@@ -125,20 +125,20 @@ void tlsf_init() {
         for (int j = 0; j < TLSF_SL_INDEX_COUNT; ++j) {
             tlsf_allocator.sl_list[i][j] = NULL;
         }
+        tlsf_allocator.fl_locks[i] = NULL;
+        tlsf_allocator.fl_unlocks[i] = NULL;
     }
     tlsf_allocator.fl_bitmap = 0;
     for (int i = 0; i < TLSF_FL_INDEX_COUNT; ++i) {
         tlsf_allocator.sl_bitmap[i] = 0;
     }
-
     tlsf_add_pool_internal(memory_pool, POOL_SIZE_BYTES);
 }
 
 void* tlsf_alloc(size_t size) {
     if (size == 0) return NULL;
 
-    size_t adjusted_size = size + sizeof(blk_hdr_t) + sizeof(size_t);
-    adjusted_size = (adjusted_size + (__alignof__(max_align_t) - 1)) & ~(__alignof__(max_align_t) - 1);
+    size_t adjusted_size = (size + sizeof(blk_hdr_t) + (__alignof__(max_align_t) - 1)) & ~(__alignof__(max_align_t) - 1);
     if (adjusted_size < MIN_BLOCK_SIZE) adjusted_size = MIN_BLOCK_SIZE;
 
     int fl, sl;
@@ -155,6 +155,8 @@ void* tlsf_alloc(size_t size) {
         fl_index = __builtin_ffs(fl_mask & (~0U << (fl_index + 1))) - 1;
         sl_index = __builtin_ffs(tlsf_allocator.sl_bitmap[fl_index]) - 1;
     } else sl_index = __builtin_ffs(sl_mask) - 1;
+    
+    if (tlsf_allocator.fl_locks[fl_index]) tlsf_allocator.fl_locks[fl_index]();
 
     blk_hdr_t* blk = tlsf_allocator.sl_list[fl_index][sl_index];
     rm_blk_from_free_ls(blk);
@@ -165,91 +167,69 @@ void* tlsf_alloc(size_t size) {
     } else {
         adjusted_size = blk->size;
     }
-
-    size_t* magic_end_ptr = (size_t*)((char*)blk + blk->size - sizeof(size_t));
-    *magic_end_ptr = TLSF_MAGIC;
-
-    return (void*)((char*)blk + sizeof(blk_hdr_t));
-}
-
-void* tlsf_alloc_ali(size_t size, size_t alignment) {
-    if (size == 0) return NULL;
-
-    size_t required_size = size + sizeof(blk_hdr_t) + sizeof(size_t) + (alignment - 1);
-
-    int fl, sl;
-    find_free_ls(required_size, &fl, &sl);
-
-    int fl_index = fl;
-    uint32_t fl_mask = tlsf_allocator.fl_bitmap & (~0U << fl);
-    if (fl_mask == 0) return NULL;
-    fl_index = __builtin_ffs(fl_mask) - 1;
-
-    int sl_index = sl;
-    uint32_t sl_mask = tlsf_allocator.sl_bitmap[fl_index] & (~0U << sl);
-    if (sl_mask == 0) {
-        fl_index = __builtin_ffs(fl_mask & (~0U << (fl_index + 1))) - 1;
-        sl_index = __builtin_ffs(tlsf_allocator.sl_bitmap[fl_index]) - 1;
-    } else sl_index = __builtin_ffs(sl_mask) - 1;
-
-    blk_hdr_t* blk = tlsf_allocator.sl_list[fl_index][sl_index];
-    rm_blk_from_free_ls(blk);
-
-    char* aligned_ptr = (char*)((uintptr_t)((char*)blk + sizeof(blk_hdr_t) + sizeof(size_t) + alignment - 1) & ~(alignment - 1));
-    size_t aligned_blk_size = (size_t)(aligned_ptr - (char*)blk + size + sizeof(size_t));
     
-    if (blk->size >= aligned_blk_size + MIN_BLOCK_SIZE) {
-        blk_hdr_t* leftover_blk = split_blk(blk, aligned_blk_size);
-        add_blk_to_free_ls(leftover_blk);
-    } else {
-        aligned_blk_size = blk->size;
-    }
-
-    size_t* magic_end_ptr = (size_t*)((char*)blk + blk->size - sizeof(size_t));
-    *magic_end_ptr = TLSF_MAGIC;
-    return (void*)aligned_ptr;
+    if (tlsf_allocator.fl_unlocks[fl_index]) tlsf_allocator.fl_unlocks[fl_index]();
+    
+    return (void*)((char*)blk + sizeof(blk_hdr_t));
 }
 
 void tlsf_free(void* p) {
     if (p == NULL) return;
 
     blk_hdr_t* blk = (blk_hdr_t*)((char*)p - sizeof(blk_hdr_t));
-    size_t* magic_end_ptr = (size_t*)((char*)p + blk->size - sizeof(blk_hdr_t) - sizeof(size_t));
+    
+    int fl, sl;
+    find_free_ls(blk->size, &fl, &sl);
 
-    if (*magic_end_ptr != TLSF_MAGIC) return;
+    if (tlsf_allocator.fl_locks[fl]) tlsf_allocator.fl_locks[fl]();
 
     blk_hdr_t* merged_blk = coalesce_blk(blk);
     add_blk_to_free_ls(merged_blk);
+
+    if (tlsf_allocator.fl_unlocks[fl]) tlsf_allocator.fl_unlocks[fl]();
 }
 
-void tlsf_init_lk(void (*lk)(void), void (*unlk)(void)) {
-    _tlsf_lock = lk;
-    _tlsf_unlock = unlk;
+void tlsf_init_lk(void (*lk)(int), void (*unlk)(int)) {
+    for (int i = 0; i < TLSF_FL_INDEX_COUNT; ++i) {
+        tlsf_allocator.fl_locks[i] = lk;
+        tlsf_allocator.fl_unlocks[i] = unlk;
+    }
     tlsf_init();
 }
 
-void tlsf_add_pool_lk(void* mem_start, size_t mem_size) {
-    if (_tlsf_lock) _tlsf_lock();
+void tlsf_add_pool_lk(void* mem_start, size_t mem_size, int fl_index) {
+    if (tlsf_allocator.fl_locks[fl_index]) tlsf_allocator.fl_locks[fl_index](fl_index);
     tlsf_add_pool_internal(mem_start, mem_size);
-    if (_tlsf_unlock) _tlsf_unlock();
+    if (tlsf_allocator.fl_unlocks[fl_index]) tlsf_allocator.fl_unlocks[fl_index](fl_index);
 }
 
 void* tlsf_alloc_lk(size_t size) {
-    if (_tlsf_lock) _tlsf_lock();
-    void* p = tlsf_alloc(size);
-    if (_tlsf_unlock) _tlsf_unlock();
-    return p;
-}
+    int fl, sl;
+    find_free_ls(size, &fl, &sl);
 
-void* tlsf_alloc_ali_lk(size_t size, size_t alignment) {
-    if (_tlsf_lock) _tlsf_lock();
-    void* p = tlsf_alloc_ali(size, alignment);
-    if (_tlsf_unlock) _tlsf_unlock();
+    int fl_index = fl;
+    uint32_t fl_mask = tlsf_allocator.fl_bitmap & (~0U << fl);
+    if (fl_mask == 0) return NULL;
+    fl_index = __builtin_ffs(fl_mask) - 1;
+    
+    if (tlsf_allocator.fl_locks[fl_index]) tlsf_allocator.fl_locks[fl_index](fl_index);
+
+    void* p = tlsf_alloc(size);
+
+    if (tlsf_allocator.fl_unlocks[fl_index]) tlsf_allocator.fl_unlocks[fl_index](fl_index);
+    
     return p;
 }
 
 void tlsf_free_lk(void* p) {
-    if (_tlsf_lock) _tlsf_lock();
+    if (p == NULL) return;
+    
+    blk_hdr_t* blk = (blk_hdr_t*)((char*)p - sizeof(blk_hdr_t));
+
+    int fl, sl;
+    find_free_ls(blk->size, &fl, &sl);
+    
+    if (tlsf_allocator.fl_locks[fl]) tlsf_allocator.fl_locks[fl](fl);
     tlsf_free(p);
-    if (_tlsf_unlock) _tlsf_unlock();
+    if (tlsf_allocator.fl_unlocks[fl]) tlsf_allocator.fl_unlocks[fl](fl);
 }
