56602f213abda7b6b93fb75aa3bfe3ad139252ef
Pr09rammerali9 <hakamiali197@gmail.com>
2025-08-20 11:33:50 +0300

    Update alloc.h

--- alloc.h
+++ alloc.h
@@ -36,14 +36,16 @@ void pbinit_lk(void (*lk)(void), void (*unlk)(void));
 void *pballoc_lk(size_t size);
 void pbfree_lk(void *p);
 
-void tlsf_init(void);
-void tlsf_init_lk(void (*lk)(void), void (*unlk)(void));
-void *tlsf_alloc(size_t size);
-void *tlsf_alloc_lk(size_t size);
-void *tlsf_alloc_ali(size_t size, size_t alignment);
-void *tlsf_alloc_ali_lk(size_t size, size_t alignment);
-void tlsf_free(void *p);
-void tlsf_free_lk(void *p);
-void tlsf_add_pool(void *mem_start, size_t mem_size);
+void tlsf_init();
+void tlsf_add_pool(void* mem_start, size_t mem_size);
+void* tlsf_alloc(size_t size);
+void* tlsf_alloc_ali(size_t size, size_t alignment);
+void tlsf_free(void* p);
+void tlsf_init_lk(void (*lk)(int), void (*unlk)(int));
+void tlsf_add_pool_lk(void* mem_start, size_t mem_size, int fl_index);
+void* tlsf_alloc_lk(size_t size);
+void* tlsf_alloc_ali_lk(size_t size, size_t alignment);
+void tlsf_free_lk(void* p);
+
 
 #endif
